0540b2a035a232938fa194c508e32d03df762425
Matteo Natalini <98880797+Matteo-Natalini@users.noreply.github.com>
2023-04-13 15:55:41 +0100

    Add files via upload

---
+++ my_first_code.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This is my first line of code
+print ("My name is Matteo Natalini")
+print ("The date is 13/04/2023")
+
+
+# In[3]:
+
+
+# Write a program to analysis real estate data
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+print (a + b + c + d + e)
+print ((a+ b + c + d + e) / 5)
+print (c - 6500)
+
+
+# In[ ]:
+
+
+
+
